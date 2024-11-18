93170abc7e41d25a228da8aa25988e469cd985bf
pruthvigururaj <44680769+pruthvigururaj@users.noreply.github.com>
2024-11-18 17:12:40 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin:[process.env.FRONTEND_URL],
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization"
   }));
